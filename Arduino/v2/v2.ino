a9083497f4424faecc0fffa2352af984691b81ea
Monte Mishkin <montem6@gmail.com>
2015-02-24 21:55:41 -0800

    Arduino: re-inserted example code's numerous output options for MPU data

--- Arduino/v2/v2.ino
+++ Arduino/v2/v2.ino
@@ -16,6 +16,11 @@
 #include "MPU6050_6Axis_MotionApps20.h"
 #include "Wire.h"
 
+
+// uncomment to see actual quaternion components 
+// in [w, x, y, z] format (not best for parsing on remote host like Processing)
+//#define OUTPUT_READABLE_QUATERNION
+
 // uncomment to see Euler angles (in degrees)
 // calculated from quaternions coming from FIFO.
 #define OUTPUT_READABLE_EULER
@@ -25,6 +30,20 @@
 // Note this also requires gravity vector calculations.
 //#define OUTPUT_READABLE_YAWPITCHROLL
 
+// uncomment to see acceleration components with gravity removed. 
+// This acceleration reference frame is not compensated for orientation, 
+// so +X is always +X according to the sensor, just without the effects of gravity.
+// use OUTPUT_READABLE_WORLDACCEL to get acceleration compensated for orientation.
+//#define OUTPUT_READABLE_REALACCEL
+
+// uncomment to see acceleration components with gravity removed 
+// and adjusted for the world frame of reference 
+// (yaw is relative to initial orientation, since no magnetometer).
+//#define OUTPUT_READABLE_WORLDACCEL
+
+// uncomment for output that matches format used for InvenSense teapot demo
+//#define OUTPUT_TEAPOT
+
 // uncomment to use bluetooth
 //#define USING_BLUETOOTH
 
@@ -32,7 +51,6 @@
 #define LED_PIN 13
 #define PHOTO_PIN A0
 
-bool blinkState = false;
 
 // orientation/motion vars
 //-----------------------------------------------------------------------
@@ -50,12 +68,15 @@ VectorFloat gravity;
 float euler[3];
 // [yaw, pitch, roll] yaw/pitch/roll container and gravity vector
 float ypr[3];
-// light sensor reading
-float photo = 1024;
 // packet structure for InvenSense teapot demo
 uint8_t teapotPacket[14] = {
   '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n'
 };
+// light sensor reading
+float photo = 1024;
+// blink state of LED for indicating status
+bool blinkState = false;
+
 
 
 // MPU control/status vars
@@ -218,6 +239,19 @@ void loop() {
     // (this lets us immediately read more without waiting for interrupt)
     fifoCount -= packetSize;
 
+    #ifdef OUTPUT_READABLE_QUATERNION
+      // display quaternion values in easy matrix form: w x y z
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      Serial.print("quat\t");
+      Serial.print(q.w);
+      Serial.print("\t");
+      Serial.print(q.x);
+      Serial.print("\t");
+      Serial.print(q.y);
+      Serial.print("\t");
+      Serial.println(q.z);
+    #endif
+
     #ifdef OUTPUT_READABLE_EULER
       // display Euler angles in degrees
       mpu.dmpGetQuaternion(&q, fifoBuffer);
@@ -243,6 +277,51 @@ void loop() {
       Serial.print(ypr[1] * 180/M_PI);
       Serial.print("\t");
       Serial.println(ypr[2] * 180/M_PI);
+    #endif
+
+    #ifdef OUTPUT_READABLE_REALACCEL
+      // display real acceleration, adjusted to remove gravity
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetAccel(&aa, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+      Serial.print("areal\t");
+      Serial.print(aaReal.x);
+      Serial.print("\t");
+      Serial.print(aaReal.y);
+      Serial.print("\t");
+      Serial.println(aaReal.z);
+    #endif
+
+    #ifdef OUTPUT_READABLE_WORLDACCEL
+      // display initial world-frame acceleration, adjusted to remove gravity
+      // and rotated based on known orientation from quaternion
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetAccel(&aa, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+      mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+      Serial.print("aworld\t");
+      Serial.print(aaWorld.x);
+      Serial.print("\t");
+      Serial.print(aaWorld.y);
+      Serial.print("\t");
+      Serial.println(aaWorld.z);
+    #endif
+
+    #ifdef OUTPUT_TEAPOT
+      // display quaternion values in InvenSense Teapot demo format:
+      teapotPacket[2] = fifoBuffer[0];
+      teapotPacket[3] = fifoBuffer[1];
+      teapotPacket[4] = fifoBuffer[4];
+      teapotPacket[5] = fifoBuffer[5];
+      teapotPacket[6] = fifoBuffer[8];
+      teapotPacket[7] = fifoBuffer[9];
+      teapotPacket[8] = fifoBuffer[12];
+      teapotPacket[9] = fifoBuffer[13];
+      Serial.write(teapotPacket, 14);
+      // packetCount, loops at 0xFF on purpose
+      teapotPacket[11]++;
     #endif
 
     // blink LED to indicate activity
